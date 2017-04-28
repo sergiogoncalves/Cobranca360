5b58d2e9fe8ab5543196069f4e350ce9e212f793
Sérgio Gonçalves <eu.sergiogoncalves@gmail.com>
2017-04-28 11:33:22 -0400

    Update

--- Knoledge/CursoJavaScript/public_html/JS/principal.js
+++ Knoledge/CursoJavaScript/public_html/JS/principal.js
@@ -5,7 +5,23 @@
  */
 
 var titulo = document.querySelector("h1");
-console.log(titulo);
-console.log(titulo.textContent);
-
 titulo.textContent = "Cleciane";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso   = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+var tdImc    = paciente.querySelector(".info-imc");
+
+var peso   = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var imc = peso /(altura * altura);
+
+tdImc.textContent = imc;
+
+console.log(paciente);
+console.log(tdPeso);
+console.log(peso);
+console.log(altura);
+console.log(imc);
